d0f9c88236a28b5f29e24307de5adc78088e352d
jahidul17 <41232668+jahidul17@users.noreply.github.com>
2026-01-19 23:55:54 +0600

    add billing admin

--- billing/admin.py
+++ billing/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from .models import Bill
 
-# Register your models here.
+@admin.register(Bill)
+class BillAdmin(admin.ModelAdmin):
+    list_display = (
+        'member',
+        'month',
+        'total_amount',
+        'payment_status',
+        'created_at',
+    )
+
+    list_filter = (
+        'month',
+        'payment_status',
+    )
+
+    search_fields = (
+        'member__full_name',
+        'member__seat_number',
+    )
+
+    readonly_fields = (
+        'seat_rent_amount',
+        'meal_bill_amount',
+        'electricity_amount',
+        'water_amount',
+        'utility_amount',
+        'total_amount',
+        'created_at',
+    )
+
+    ordering = ('-month',)
+
+    fieldsets = (
+        ("Member Info", {
+            'fields': ('member', 'month')
+        }),
+        ("Bill Breakdown", {
+            'fields': (
+                'seat_rent_amount',
+                'meal_bill_amount',
+                'electricity_amount',
+                'water_amount',
+                'utility_amount',
+                'total_amount',
+            )
+        }),
+        ("Payment", {
+            'fields': ('payment_status',)
+        }),
+        ("System Info", {
+            'fields': ('created_at',)
+        }),
+    )
+
+
+
